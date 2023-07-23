bad0c3afa752c9f06285beac62d041bdf52c09a3
Joshua Ubani-Wokoma <joshubaniwokoma@gmail.com>
2023-07-23 19:34:03 +0100

    add bcrypt

--- services/auth.services.js
+++ services/auth.services.js
@@ -1,14 +1,17 @@
+import bycrypt from "bcrypt"
 import User from "../models/user.model.js"
 
 export async function checkUser(userObject) {
     try {
-        const result = await User.findOne({username: userObject.username, password: userObject.password})
+        const result = await User.findOne({username: userObject.username})
 
-        if(result) {
-            return true
+        if(!result) return false
+
+        const found = await bycrypt.compare(userObject.password, result.password)
+
+        if(found) {
+            return true    
         }
-        
-        return false
     }
     catch(error) {
         throw error
@@ -17,7 +20,14 @@ export async function checkUser(userObject) {
 
 export async function saveUser(userObject) {
     try {
-        const user = new User(userObject)
+        const saltRounds = 10
+
+        const hashedPassword = await bycrypt.hash(userObject.password, saltRounds)
+
+        const user = new User({
+            password: hashedPassword,
+            ...userObject,
+        })
 
         await user.save()
     }
