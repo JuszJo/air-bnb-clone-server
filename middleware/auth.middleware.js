e575707442b615e2cd25fe24eee0a7b2f85a7c61
Joshua Ubani-Wokoma <joshubaniwokoma@gmail.com>
2023-07-23 23:04:46 +0100

    fix auth middleware

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -7,14 +7,21 @@ export default function verifyToken(req, res, next) {
     
     const token = req.headers["x-access-token"]
 
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET)
-
-        next()
-    }
-    catch(error) {
+    if(!token) {
         res.status(400).json({
-            status: "invalid token"
+            status: "unauthorized"
         })
+    }
+    else {
+        try {
+            const decoded = jwt.verify(token, process.env.JWT_SECRET)
+    
+            next()
+        }
+        catch(error) {
+            res.status(400).json({
+                status: "invalid token"
+            })
+        }
     }
 }
--- routes/routes.js
+++ routes/routes.js
@@ -9,7 +9,8 @@ export default function useRoutes(app) {
     app.use((req, res, next) => {
         res.set({
             "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "*"
+            "Access-Control-Allow-Methods": "*",
+            "Access-Control-Allow-Headers": "*"
         })
 
         next()
