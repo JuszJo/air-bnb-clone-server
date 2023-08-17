bed46416e3d2abbb36684562b84eb7224c2077d5
Joshua Ubani-Wokoma <joshubaniwokoma@gmail.com>
2023-08-17 14:32:17 +0100

    fix loading issue

--- middleware/cors.middleware.js
+++ middleware/cors.middleware.js
@@ -1,6 +1,6 @@
 export function cors(req, res, next) {
     res.set({
-        "Access-Control-Allow-Origin": "http://localhost:5173",
+        "Access-Control-Allow-Origin": "https://jobnb.netlify.app/",
         "Access-Control-Allow-Methods": "GET, POST",
         "Access-Control-Allow-Headers": "content-type, authorization",
     })
@@ -10,7 +10,7 @@ export function cors(req, res, next) {
 
 export function corsOptions(req, res) {
     res.set({
-        "Access-Control-Allow-Origin": "http://localhost:5173",
+        "Access-Control-Allow-Origin": "https://jobnb.netlify.app/",
         "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
         "Access-Control-Allow-Headers": "content-type, authorization",
         "Access-Control-Allow-Credentials": "true"
