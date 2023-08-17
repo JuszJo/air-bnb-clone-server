7b4bb4645a4c8721b76571565c8d0e3b465d3e55
Joshua Ubani-Wokoma <joshubaniwokoma@gmail.com>
2023-08-17 01:53:18 +0100

    remove unneccessary json from listing

--- controllers/listing.controller.js
+++ controllers/listing.controller.js
@@ -4,9 +4,7 @@ export async function getListing(req, res) {
     const result = await Listings.findOne({ _id: req.params.id })
 
     if(result) {
-        res.status(200).json({
-            result
-        })
+        res.status(200).json({...result})
     }
     else {
         res.status(404).json({
