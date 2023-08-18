eb095dbd5bab12aa8e06a2d2fa3d52f40237f8cf
Joshua Ubani-Wokoma <joshubaniwokoma@gmail.com>
2023-08-18 14:37:20 +0100

    add delete method

--- middleware/cors.middleware.js
+++ middleware/cors.middleware.js
@@ -3,7 +3,7 @@ const prod = true
 export function cors(req, res, next) {
     res.set({
         "Access-Control-Allow-Origin": prod ? "https://jobnb.netlify.app" : "http://localhost:5173",
-        "Access-Control-Allow-Methods": "GET, POST",
+        "Access-Control-Allow-Methods": "GET, POST, DELETE",
         "Access-Control-Allow-Headers": "content-type, authorization",
     })
 
@@ -13,7 +13,7 @@ export function cors(req, res, next) {
 export function corsOptions(req, res) {
     res.set({
         "Access-Control-Allow-Origin": prod ? "https://jobnb.netlify.app" : "http://localhost:5173",
-        "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+        "Access-Control-Allow-Methods": "GET, POST, DELETE, OPTIONS",
         "Access-Control-Allow-Headers": "content-type, authorization",
         "Access-Control-Allow-Credentials": "true"
     })
